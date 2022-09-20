2c8076c33226eca1815f100d40359bfe300ebdbe
MaX <kingofmadnes@gmail.com>
2022-09-20 22:18:05 +0300

    made presentetion for webdite html/css/js

---
+++ client/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <p> More to come!</p>
+</template>
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+<style scoped>
+    p {
+    font-size: 2.5em;
+    text-align: center;
+    }
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import HomeVue from '../components/Home.vue';
 </script>
 
 <template>
