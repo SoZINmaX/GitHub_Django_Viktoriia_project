973fc0236b9a401c1f13b0bc907f753100a01ac0
MaX <kingofmadnes@gmail.com>
2022-09-21 20:08:20 +0300

    changes to presentation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue';
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <!-- <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-  </header>
-
-  <RouterView /> -->
   <HomeView />
 </template>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,9 @@
+import './assets/base.min.css'
+import './assets/assets/agency.js'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import './assets/base.min.css'
 
 const app = createApp(App)
 
